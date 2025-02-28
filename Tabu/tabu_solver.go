97e003cd1c976a4ac2b95f780ad7f0bed82ab5c5
Zhuoyi-The-Wang <109244989+Zhuoyi-The-Wang@users.noreply.github.com>
2025-02-27 19:57:17 -0700

    994 temp

--- Tabu/tabu_solver.go
+++ Tabu/tabu_solver.go
@@ -811,7 +811,7 @@ func main() {
 	// Tabu search parameters
 	maxIter := 2500
 	tabuTenure := 50
-	neighborhoodSize := 100
+	neighborhoodSize := 50
 
 	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
 	bestSol := constructTabuSolution(g, initSol, maxIter, tabuTenure, neighborhoodSize, rnd)
