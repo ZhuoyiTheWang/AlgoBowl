a781b7585a53372fad9ae1653b8244fd73ad1b2b
Brian.Tandy <btandy@mines.edu>
2025-02-27 21:14:18 -0700

    Brian's best

--- check_submissions.sh
+++ check_submissions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-submissions_folder="./all_outputs/submitted"
+submissions_folder="./all_outputs/best_performance"
 input_folder="./algobowl_inputs"
 combined_results="./submission_verification_results.txt"
 
@@ -13,7 +13,7 @@ touch "$combined_results"
 for submission_file in "$submissions_folder"/*; do
     filename=$(basename "$submission_file")
     echo "Checking ${submission_file}"
-    input_number="${filename:19}"
+    input_number="${filename:18}"
     input_file="${input_folder}/input_group${input_number}"
     python ./Python_Verification.py "$input_file" "$submission_file" >> "$combined_results" 2>&1
     
